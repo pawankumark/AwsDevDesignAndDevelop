0359b407e1eef15c420780322d3d7f5d8ba2a622
Marcelo Barsotti <mbarsott@gmail.com>
2020-02-17 21:11:24 -0500

    3.8. Managing EC2 Instances in AWS

--- scripts/module_02/manage-ec2-instance.js
+++ scripts/module_02/manage-ec2-instance.js
@@ -1,20 +1,37 @@
 // Imports
-// TODO: Import the aws-sdk
+const AWS = require("aws-sdk");
 
-// TODO: Configure region
+AWS.config.update({ region: "us-east-1" });
 
 // Declare local variables
-// TODO: Create an ec2 object
+const ec2 = new AWS.EC2();
 
-function listInstances () {
-  // TODO: List instances using ec2.describeInstances()
+function listInstances() {
+  return new Promise((resolve, reject) => {
+    ec2.describeInstances({}, (err, data) => {
+      if (err) PromiseRejectionEvent(err);
+      else {
+        resolve(
+          data.Reservations.reduce((i, r) => {
+            return i.concat(r.Instances);
+          }, [])
+        );
+      }
+    });
+  });
 }
 
-function terminateInstance (instanceId) {
-  // TODO: Terminate an instance with a given instanceId
+function terminateInstance(instanceId) {
+  const params = {
+    InstanceIds: [instanceId]
+  };
+  return new Promise((resolve, reject) => {
+    ec2.terminateInstances(params, (err, data) => {
+      if (err) reject(err);
+      else resolve(data);
+    });
+  });
 }
 
-listInstances()
-.then(data => console.log(data))
-// terminateInstance()
-// .then(data => console.log(data))
+// listInstances().then(data => console.log(data));
+terminateInstance("i-0fbc9fd15325e8cdc").then(data => console.log(data));
